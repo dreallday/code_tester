5d264f500dbc2694bb1019c99b58e44fce4946f0
Andrei Fedianov <andreifaa@gmail.com>
2017-08-31 15:02:34 -0400

    New Items

--- config/dev.exs
+++ config/dev.exs
@@ -49,10 +49,10 @@ config :logger, :console, format: "[$level] $message\n"
 config :phoenix, :stacktrace_depth, 20
 
 # Configure your database
-config :code_tester, CodeTester.Repo,
-  adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
-  database: "code_tester_dev",
-  hostname: "localhost",
-  pool_size: 10
+# config :code_tester, CodeTester.Repo,
+#   adapter: Ecto.Adapters.Postgres,
+#   username: "postgres",
+#   password: "postgres",
+#   database: "code_tester_dev",
+#   hostname: "localhost",
+#   pool_size: 10
